ec25754917f0a43527b5c095e40e10fda3f368aa
nico18576626619 <274250917@qq.com>
2017-10-27 18:11:02 +0800

    优化配置文件，保持类名和数据文件名一致则无需再配置文件中配置数据文件目录

--- SeleniumWebRegression/src/com/demo/classTest.java
+++ SeleniumWebRegression/src/com/demo/classTest.java
@@ -1,12 +1,34 @@
 package com.demo;
 
+import java.io.File;
+import java.io.IOException;
+
 import com.common.Utils;
 
 public class classTest {
-	public static void main(String[] args){
-		System.out.println(Utils.getCurClassName());
+	public static void main(String[] args) throws IOException{
+		File f=getCaseFile();
+		System.out.println(f.getName());
 		
 	}
-
+	
+	private static File getCaseFile(){
+		String path="./data/"+getCurClassName()+".xlsx";
+		File f=new File(path);
+		if (!f.exists()){
+			throw new AssertionError("找不到测试用例数据文件"+getCurClassName()+".xlsx");
+		}
+		return f;
+	}
+	
+	
+	private static String getCurClassName() {
+		// TODO Auto-generated method stub
+		String classname=Thread.currentThread().getStackTrace()[1].getClassName();
+		int temp=classname.lastIndexOf(".");
+		String sname=classname.substring(temp+1);
+		return sname;
+	}
+	
 
 }
---
+++ SeleniumWebRegression/src/com/testng/Baisearch.java
@@ -0,0 +1,142 @@
+package com.testng;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.common.Utils;
+@SuppressWarnings("unused")
+public class Baisearch {
+	
+	WebDriver driver;
+	int waitTime = 60;
+	String BrowserFlag="";
+	Properties props;
+	
+	@Parameters({"config"})
+	@Test
+	public void Start(String config)
+	{
+		try {
+			URL fileurl = ClassLoader.getSystemClassLoader().getResource(config);
+			FileInputStream fin = new FileInputStream(fileurl.getFile());
+			props = new Properties();
+			props.load(fin);
+	
+			File xlsfile = getCaseFile();
+			  try
+				 {
+				  	CreateChromeDriver();
+				  	ServiceLogin(xlsfile,"search","search");
+				
+	
+				 }catch(Exception e)
+				 {
+					 MyTakesScreenshot();
+					 Assert.fail("异常"+e.getMessage());
+				 }
+		} catch (Exception e) {
+			System.out.println("load configrations fail...");
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	
+	private void ServiceLogin(File xlsfile,String sheet,String caseName) throws Exception
+	{
+		String url = props.getProperty("Baidu.url");
+		Utils.SysLogin(driver, url, BrowserFlag, xlsfile,sheet,caseName);
+	}
+	
+	private void TestExec(File testdata,String sheetname,String caseName) throws Exception {
+		List<String[]> dataList = Utils.getDataFormxlsxByCaseName(testdata, sheetname, caseName);
+		Utils.SetElementValue(driver, dataList);	
+	}
+	
+	
+	public  void MyTakesScreenshot() {
+		String logFolder = props.getProperty("logfolder");
+		if(logFolder.equals("")) 
+			logFolder ="d:/logs";
+	      if (!(new File(logFolder).isDirectory())) {
+	         new File(logFolder).mkdir();  
+	      }
+	     
+	      SimpleDateFormat simDate = new SimpleDateFormat("yyyy-MM-dd");
+	      String dateFolder =logFolder+File.separator+simDate.format(new Date());
+	      if (!(new File(dateFolder).isDirectory())) {
+		         new File(dateFolder).mkdir();
+		      }
+
+		  SimpleDateFormat sdf = new SimpleDateFormat("HH-mm-ss");
+	      String time = sdf.format(new Date()); 
+	      File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); 
+	      try { 
+	             FileUtils.copyFile(scrFile, new File(dateFolder + File.separator + this.getClass().getName()+"_"+time + ".jpg")); 
+	          } catch (IOException e) { 
+	               e.printStackTrace(); 
+	           } 
+	    }
+	
+	private void CreateChromeDriver()
+	{
+		String ChDriverServer = props.getProperty("ch.driver");
+		System.setProperty("webdriver.chrome.driver", ChDriverServer);
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	private void CreateHtmlUnitDriver(){
+		driver = new HtmlUnitDriver();
+		driver.manage().window().maximize();
+	}
+	
+	private static File getCaseFile(){
+		String path="./data/"+getCurClassName()+".xlsx";
+		File f=new File(path);
+		if (!f.exists()){
+			throw new AssertionError("找不到测试用例数据文件"+getCurClassName()+".xlsx");
+		}
+		return f;
+	}
+	
+	private static String getCurClassName() {
+		// TODO Auto-generated method stub
+		String classname=Thread.currentThread().getStackTrace()[1].getClassName();
+		int temp=classname.lastIndexOf(".");
+		String sname=classname.substring(temp+1);
+		return sname;
+	}
+	
+}
